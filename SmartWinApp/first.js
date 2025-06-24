0c4497d33703ea35e2db3fd4fb84e1c3989f5f3f
Kanika  Im-erb <141254728+Kanikany2003@users.noreply.github.com>
2025-06-24 16:35:51 +0700

    Add files via upload

---
+++ SmartWinApp/first.js
@@ -0,0 +1,23 @@
+const drivers = [
+    {
+        id: "12345",
+        name: "นายเสก โมเม",
+        location: "วิน หน้าโรงเรียนบางชัน",
+        shirt: "20",
+        license: "กท-2431"
+    },
+    {
+        id: "67890",
+        name: "น.ส.แพร พลอย",
+        location: "MRT ลาดพร้าว",
+        shirt: "15",
+        license: "ขย-1122"
+    },
+    {
+        id: "45678",
+        name: "นายวันใหม่ ใจดี",
+        location: "BTS อุดมสุข",
+        shirt: "08",
+        license: "ขข-5678"
+    }
+];
